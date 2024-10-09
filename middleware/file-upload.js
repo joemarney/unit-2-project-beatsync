c84c184b9910e10eadfffe0a1fd904161452edb1
joemarney <marney.joe@gmail.com>
2024-10-09 09:35:44 +0100

    added cloudinary for uploads

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,12 +1,23 @@
 const multer = require("multer");
+const path = require("path");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname.replace(/ /g, "-"));
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads",
+    allowed_formats: ["jpg", "png", "jpeg"],
+    public_id: (req, file) => {
+      return Date.now() + "-" + path.parse(file.originalname).name.replace(/ /g, "-");
+    },
   },
 });
 
-const upload = multer({ storage: storage });
-
-module.exports = upload;
+module.exports = multer({ storage: storage });
